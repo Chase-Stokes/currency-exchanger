b02bf01beb67e399121a47a7f88166a42a4c90c0
Chase-Stokes <chasehstokes@gmail.com>
2021-11-19 13:17:19 -0800

    wip module errors

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import curEx from './c-ex.js';
 
-// function conversion(response){
-//   if (response.conversion_rates[0]){
-//     $("#cExOutput").text()
-//   }
+function conversion(response){
+  let userInput = $("#USD").val();
+  let userInput2 = $("#pickCurrency").val();
+  if (response.conversion_rates[0] === userInput2){
+    $("#cExOutput").text(userInput * userInput2)
+  }
 
-// }
+}
 
 $(document).ready(function() {
   $('#formOne').submit(function(event) {
     event.preventDefault();
     let currency = $('#pickCurrency').val();
+
     curEx.currencyEx(currency);
       .then(function(response) {
         conversion(response);
--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         }
       },
       {
-        test:/\.html$/i,
+        test:/\.html$/,
         use: [
           'html-loader'
         ]
